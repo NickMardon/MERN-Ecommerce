b77a66b1c77d7bd1520699d0c6be9a081aefe46c
Nick Mardon <nickmardondigital@gmail.com>
2021-02-01 15:05:44 -0800

    created user and product models

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from "express"
+import dotenv from "dotenv"
+import colors from 'colors'
 import connectDB from './config/db.js'
-import products from "./data/products.js";
+import products from "./data/products.js"
 
 dotenv.config();
 
@@ -26,5 +27,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   5000,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
