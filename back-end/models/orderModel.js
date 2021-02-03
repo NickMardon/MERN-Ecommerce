d0a19db6076a0710c7642699b1da063eef3d4b65
Nick Mardon <nickmardondigital@gmail.com>
2021-02-02 20:10:31 -0800

    seeded database with seeder js

--- back-end/models/orderModel.js
+++ back-end/models/orderModel.js
@@ -1,11 +1,11 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from 'mongoose'
 
 const orderSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: "User",
+      ref: 'User',
     },
     orderItems: [
       {
@@ -16,7 +16,7 @@ const orderSchema = mongoose.Schema(
         product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
-          ref: "Product",
+          ref: 'Product',
         },
       },
     ],
@@ -27,14 +27,14 @@ const orderSchema = mongoose.Schema(
       country: { type: String, required: true },
     },
     paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
       id: { type: String },
       status: { type: String },
       update_time: { type: String },
       email_address: { type: String },
-    },
-    paymentResult: {
-      type: String,
-      required: true,
     },
     taxPrice: {
       type: Number,
@@ -71,8 +71,8 @@ const orderSchema = mongoose.Schema(
   {
     timestamps: true,
   }
-);
+)
 
-const Order = mongoose.model("Order", orderSchema);
+const Order = mongoose.model('Order', orderSchema)
 
-export default Order;
+export default Order
