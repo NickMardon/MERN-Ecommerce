dc6c13858a61349cc87d396f776c68b34fd889e8
Nick Mardon <nickmardondigital@gmail.com>
2021-02-10 16:02:16 -0800

    custom error handling

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,14 @@
-import express from "express"
-import dotenv from "dotenv"
-import colors from 'colors'
-import connectDB from './config/db.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from "./config/db.js";
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
-connectDB()
+connectDB();
 
 const app = express();
 
@@ -15,12 +16,17 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes);
 
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   5000,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
