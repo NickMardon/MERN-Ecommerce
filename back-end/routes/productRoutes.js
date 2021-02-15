e01f9a37199b9e3534b0f5fd82d0d3adf5b7ca07
Nick Mardon <nickmardondigital@gmail.com>
2021-02-14 16:15:57 -0800

    converted homescreen to redux state

--- back-end/routes/productRoutes.js
+++ back-end/routes/productRoutes.js
@@ -1,28 +1,34 @@
-import e from 'express';
-import express from 'express'
-import asyncHandler from 'express-async-handler'
-const router = express.Router()
-import Product from '../models/productModel.js'
+import e from "express";
+import express from "express";
+import asyncHandler from "express-async-handler";
+const router = express.Router();
+import Product from "../models/productModel.js";
 
 // Fetch all products
-router.get("/", asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.status(401);
+    throw new Error("Not Authorized");
     res.json(products);
-  }));
-  
+  })
+);
 
 // Fetch specific product
-  router.get("/:id", asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-    if(product) {
-        res.json(product)
+    if (product) {
+      res.json(product);
     } else {
-        res.status(404)
-        throw new Error('Product not found')
+      res.status(404);
+      throw new Error("Product not found");
     }
     res.json(product);
-  }));
+  })
+);
 
-export default router
+export default router;
